2369f2fe2d5e1f975f00329998c2fdf6a33ecb65
Hemanth Balla <73847080+hemanthballa07@users.noreply.github.com>
2026-06-23 18:07:10 -0400

    test: add sendRideOffer payload and E.164 stripping coverage to WhatsApp LIVE gateway

--- backend/src/test/java/com/autodispatch/notification/internal/WhatsAppCloudApiGatewayTest.java
+++ backend/src/test/java/com/autodispatch/notification/internal/WhatsAppCloudApiGatewayTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.autodispatch.notification.api.RideOfferNotification;
+import java.math.BigDecimal;
+import java.util.UUID;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.matchingJsonPath;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
@@ -64,5 +68,39 @@ class WhatsAppCloudApiGatewayTest {
 
         wireMock.verify(1, postRequestedFor(urlPathEqualTo("/12345/messages"))
                 .withHeader("Authorization", equalTo("Bearer test-access-token")));
+    }
+
+    @Test
+    void sendRideOffer_postsInteractiveButtonPayload() {
+        wireMock.stubFor(post(urlPathEqualTo("/12345/messages"))
+                .willReturn(aResponse().withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"messages\":[{\"id\":\"wamid.offer-1\"}]}")));
+
+        UUID rideId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+        RideOfferNotification offer = new RideOfferNotification(
+                rideId, "Gate A", "Main Building", new BigDecimal("45.00"), "R-AB12", 45L);
+
+        gateway.sendRideOffer("+919800000000", offer);
+
+        wireMock.verify(1, postRequestedFor(urlPathEqualTo("/12345/messages"))
+                .withRequestBody(matchingJsonPath("$.type", equalTo("interactive")))
+                .withRequestBody(matchingJsonPath("$.interactive.type", equalTo("button")))
+                .withRequestBody(matchingJsonPath(
+                        "$.interactive.action.buttons[0].reply.id",
+                        equalTo("ACCEPT:" + rideId))));
+    }
+
+    @Test
+    void sendText_stripsLeadingPlusFromRecipient() {
+        wireMock.stubFor(post(urlPathEqualTo("/12345/messages"))
+                .willReturn(aResponse().withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"messages\":[{\"id\":\"wamid.strip-1\"}]}")));
+
+        gateway.sendText("+919800000000", "hello");
+
+        wireMock.verify(1, postRequestedFor(urlPathEqualTo("/12345/messages"))
+                .withRequestBody(matchingJsonPath("$.to", equalTo("919800000000"))));
     }
 }
